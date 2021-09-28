05c791f00d12769278fc686def203c5652f5370e
corostats <corostats@users.noreply.github.com>
2021-09-28 11:03:51 +0200

    Render only by hashchange event

--- resources/main.js
+++ resources/main.js
@@ -2,18 +2,17 @@ import en from './lang/en.js';
 import de from './lang/de.js';
 import data from './data.js';
 
-const render = () => {
-	// Read the language from the local storage
-	let language = localStorage.getItem('corostats.language');
-	if (!language) {
-		// When no language is set, use the one from the browser
-		language = window.navigator.language.substring(0, 2);
-	}
+// Listen for language clicks
+window.addEventListener('hashchange', (e) => {
+	let language = 'de';
 
 	// When hash is set update the location
 	if (location.hash.length > 1) {
 		language = location.hash.replace('#', '');
 	}
+
+	// Set the actual language in the store
+	localStorage.setItem('corostats.language', language);
 
 	// Define the translations
 	const translations = language === 'de' ? de : en;
@@ -24,13 +23,18 @@ const render = () => {
 
 	// Read the data
 	data(translations);
-}
-
-// Listen for language clicks
-window.addEventListener('hashchange', (e) => {
-	localStorage.setItem('corostats.language', location.hash.replace('#', ''));
-	render();
 });
 
-// Render initially
-render();
+if (location.hash.length <= 1) {
+	// Get the last used language from the store
+	let language = localStorage.getItem('corostats.language');
+	if (!language) {
+		// When no language is set, use the one from the browser
+		language = window.navigator.language.substring(0, 2);
+	}
+
+	// Trigger the hash change
+	location.hash = language;
+} else {
+	window.dispatchEvent(new Event('hashchange'));
+}
