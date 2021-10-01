19d9fcf1736eb9ad5851236e3c174dea36164aa5
corostats <corostats@users.noreply.github.com>
2021-10-01 13:48:29 +0200

    Vaccinated information

--- resources/data.js
+++ resources/data.js
@@ -7,15 +7,17 @@ export default function (language) {
 	//  Get the data from the API
 	api.getData().then((data) => {
 		// The total variables
+		let totalPopulation = 0;
 		let totalCases = 0;
 		let totalHosp = 0;
 		let totalDeath = 0;
+		let totalVacc = 0;
 
 		// Reset the data
 		document.querySelector('.results-years').innerHTML = '';
 
 		// Create the table for the total data
-		const table = createElement('table', 'results__table table table-striped table-responsive', '', document.querySelector('.results-years'));
+		let table = createElement('table', 'results__table table table-striped table-responsive', '', document.querySelector('.results-years'));
 
 		// Build the head section
 		let row = createElement('tr', '', '', createElement('thead', '', '', table));
@@ -23,7 +25,7 @@ export default function (language) {
 		createElement('td', 'data-cell', l.total, row);
 
 		// Build the totals
-		data.years.forEach((year) => {
+		data.years['covid'].forEach((year) => {
 			totalCases += year.cases;
 			totalHosp += year.hosp;
 			totalDeath += year.death;
@@ -31,35 +33,38 @@ export default function (language) {
 			// Create the header cell for the year
 			createElement('td', 'data-cell', year.year, row);
 		});
+		data.years['vacc'].forEach((year) => {
+			totalVacc += year.vaccinated;
+		});
 
 		// Build the body
 		const body = createElement('tbody', '', '', table);
+
 		row = createElement('tr', '', '', body);
 		createElement('td', '', l.cases, row);
 		createElement('td', 'data-cell', format(totalCases), row);
-		data.years.forEach((year) => createElement('td', 'data-cell', format(year.cases), row));
+		data.years['covid'].forEach((year) => createElement('td', 'data-cell', format(year.cases), row));
 
 		row = createElement('tr', '', '', body);
 		createElement('td', '', l.hospitalization, row);
 		createElement('td', 'data-cell', format(totalHosp), row);
-		data.years.forEach((year) => createElement('td', 'data-cell', format(year.hosp), row));
+		data.years['covid'].forEach((year) => createElement('td', 'data-cell', format(year.hosp), row));
 
 		row = createElement('tr', '', '', body);
 		createElement('td', '', l.deaths, row);
 		createElement('td', 'data-cell', format(totalDeath), row);
-		data.years.forEach((year) => createElement('td', 'data-cell', format(year.death), row));
+		data.years['covid'].forEach((year) => createElement('td', 'data-cell', format(year.death), row));
+
+		row = createElement('tr', '', '', body);
+		createElement('td', '', l.vaccinated, row);
+		createElement('td', 'data-cell', format(totalVacc), row);
+		data.years['vacc'].forEach((year) => createElement('td', 'data-cell', format(year.vaccinated), row));
 
 		// Reset the totals
 		totalCases = 0;
 		totalHosp = 0;
 		totalDeath = 0;
-
-		// BUild the totals from the stats
-		data.stats.forEach((group) => {
-			totalCases += group.cases;
-			totalHosp += group.hosp;
-			totalDeath += group.death;
-		});
+		totalVacc = 0;
 
 		// The root node for the ages boxes
 		const parent = document.querySelector('.results_ages');
@@ -67,42 +72,81 @@ export default function (language) {
 		// Reset the content
 		parent.innerHTML = '';
 
+		// The sections with tables
+		const sections = {};
+
+		// Build the totals from the stats
+		data.stats['covid'].forEach((group) => {
+			totalPopulation += group.pop;
+			totalCases += group.cases;
+			totalHosp += group.hosp;
+			totalDeath += group.death;
+
+			// Populate the ranges
+			if (sections[group.range] === undefined) {
+				const section = createElement('section', 'result col-lg-6', '', parent);
+				createElement('h3', 'result__heading', group.range != 'Unbekannt' ? group.range + ' ' + l.years : l.unknown, section);
+
+				sections[group.range] = createElement('table', 'table border', '', section);
+			}
+		});
+		data.stats['vacc'].forEach((group) => {
+			totalVacc += group.vaccinated;
+		});
+
 		// Loop over each age group
-		data.stats.forEach((group) => {
-			const section = createElement('section', 'result col-lg-6', '', parent);
-			createElement('h3', 'result__heading', group.range != 'Unbekannt' ? group.range + ' ' + l.years : l.unknown, section);
+		data.stats['covid'].forEach((group) => {
+			let row = createElement('tr', '', '', sections[group.range]);
+			createElement('td', '', l.population + ':', row);
+			createElement('td', 'data-cell', format(group.pop), row);
+			createElement('td', 'data-cell', ((100 / totalPopulation) * group.pop).toFixed(3) + '%', row);
 
-			const table = createElement('table', 'table border', '', section);
-
-			let row = createElement('tr', '', '', table);
+			row = createElement('tr', '', '', sections[group.range]);
 			createElement('td', '', l.cases + ':', row);
 			createElement('td', 'data-cell', format(group.cases), row);
 			createElement('td', 'data-cell', ((100 / totalCases) * group.cases).toFixed(3) + '%', row);
 
-			row = createElement('tr', '', '', table);
+			row = createElement('tr', '', '', sections[group.range]);
 			createElement('td', '', l.hospitalization + ':', row);
 			createElement('td', 'data-cell', format(group.hosp), row);
 			createElement('td', 'data-cell', ((100 / totalHosp) * group.hosp).toFixed(3) + '%', row);
 
-			row = createElement('tr', '', '', table);
+			row = createElement('tr', '', '', sections[group.range]);
 			createElement('td', '', l.deaths + ':', row);
 			createElement('td', 'data-cell', format(group.death), row);
 			createElement('td', 'data-cell', ((100 / totalDeath) * group.death).toFixed(3) + '%', row);
-
-			row = createElement('tr', '', '', table);
+		});
+		data.stats['vacc'].forEach((group) => {
+			let row = createElement('tr', '', '', sections[group.range]);
+			createElement('td', '', l.vaccinated + ':', row);
+			createElement('td', 'data-cell', format(group.vaccinated), row);
+			createElement('td', 'data-cell', ((100 / totalVacc) * group.vaccinated).toFixed(3) + '%', row);
+		});
+		data.stats['covid'].forEach((group) => {
+			let row = createElement('tr', '', '', sections[group.range]);
 			createElement('td', '', l.cases_to_hospitalization + ':', row);
 			createElement('td', 'data-cell', format(group.cases) + ' / ' + format(group.hosp), row);
 			createElement('td', 'data-cell', ((100 / group.cases) * group.hosp).toFixed(3) + '%', row);
 
-			row = createElement('tr', '', '', table);
+			row = createElement('tr', '', '', sections[group.range]);
 			createElement('td', '', l.cases_to_deaths + ':', row);
 			createElement('td', 'data-cell', format(group.cases) + ' / ' + format(group.death), row);
 			createElement('td', 'data-cell', ((100 / group.cases) * group.death).toFixed(3) + '%', row);
 		});
+		data.stats['vacc'].forEach((group) => {
+			let row = createElement('tr', '', '', sections[group.range]);
+			createElement('td', '', l.population_to_vaccinated + ':', row);
+			createElement('td', 'data-cell', format(group.pop) + ' / ' + format(group.vaccinated), row);
+			createElement('td', 'data-cell', (group.pop === 0 ? 0 : ((100 / group.pop) * group.vaccinated).toFixed(3)) + '%', row);
+		});
+
+		// Set the population
+		document.querySelector('.population').innerHTML = format(totalPopulation);
 
 		// Set the date
 		const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-		document.querySelector('.update_date').innerHTML = (new Date(data.date.start)).toLocaleString('de-DE', options) + ' - ' + (new Date(data.date.end)).toLocaleString('de-DE', options);
+		document.querySelector('.update_date').innerHTML = (new Date(data.date.start)).toLocaleString('de-DE', options)
+			+ ' - ' + (new Date(data.date.end)).toLocaleString('de-DE', options);
 	});
 }
 
@@ -127,9 +171,14 @@ function createElement(type, classes, content, parent) {
 
 /**
  * Formats the given number with single quotes as thousand delimiter.
+ *
  * @param Number number
  * @returns string
  */
 function format(number) {
+	// Check if it is really a number
+	if (isNaN(number)) {
+		return '0';
+	}
 	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'");
 }
--- resources/lang/de.js
+++ resources/lang/de.js
@@ -1,6 +1,6 @@
 export default {
 	title: "Corona Alterstatistiken für die Schweiz",
-	timespan: "Das BAG veröffentlicht die Zahlen immer bis zur vergangenen Woche. Die hier benutzten Daten sind von <span class=\"update_date\"></span>.",
+	timespan: "Das BAG veröffentlicht die Zahlen immer bis zur vergangenen Woche. Die hier benutzten Daten sind von <span class=\"update_date\"></span>. Die Population wird ebenfalls aus der Statistik ausgelesen und beträgt aktuell <span class=\"population\"></span> Personen.",
 	intro: "Diese Seite macht die Corona Statistiken vom <a href=\"https://www.bag.admin.ch\" target=\"_blank\">Bundesamt für Gesundheit (BAG)</a> einfach lesbar. Die folgenden Daten wurden von dem <a href=\"https://opendata.swiss/en/dataset/covid-19-schweiz\"  target=\"_blank\">offiziellen Schweizer Open Government Data Hub</a> ausgelesen und sind für alle frei verfügbar.",
 	total_title: "Total pro Jahr",
 	total_intro: "Statistiken aufsummiert pro Jahr.",
@@ -10,14 +10,17 @@ export default {
 	mortality_intro: "Die erwartete und aktuelle Sterblichkeit im Jahre 2020 - 2021 zwischen jüngeren (0 - 64) and älteren (über 64) Personen. Die Grafik ist vom <a href=\"https://www.bfs.admin.ch/bfs/en/home/statistics/health/state-health/mortality-causes-death.html\" target=\"_blank\">Schweizer Bundesamt für Statistik</a>.",
 	footer: "Diese Seite ist keine offizielle Seite vom BAG. Der Source Code dieser Seite kann eingesehen werden auf <a href=\"https://github.com/corostats/corostats\" target=\"_blank\">github.com/corostats/corostats</a>.",
 	label: {
+		population: 'Population',
 		type: "Typ",
 		total: "Total",
 		cases: "Fälle",
 		years: "Jahre",
 		hospitalization: "Hospitalisierungen",
 		deaths: "Tote",
+		vaccinated: "Geimpft",
 		cases_to_hospitalization: "Fälle zu Hospitalisierungen",
 		cases_to_deaths: "Fälle zu Tote",
+		population_to_vaccinated: "Population zu Geimpften",
 		unknown: 'Unbekannt'
 	}
 }
